cc6e887e3e92f0c11e754a6a703a16770aea5066
Lucie <64801159+Lucie-O@users.noreply.github.com>
2020-06-21 22:40:26 +0200

    Put the code drawing the maze in a function & created drawBridge function for later

--- js/level-3.js
+++ js/level-3.js
@@ -9,7 +9,7 @@ function toggleBurgerMenu(icon) {
 
 burgerIcon.addEventListener("click", function(){toggleBurgerMenu(this)});
 
-// MAZE
+// CANVAS & MAZE
 const canvas = document.querySelector("#screen")
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -42,30 +42,38 @@ var maze = [
   [4,1,4,0,0,1,4,4,4,0,0,0,0,3,3,3,3,0,3,3]
 ]
 
-for (var y = 0; y < maze.length; y++) {
-  for (var x = 0; x < maze[y].length; x++) {
-    if (maze[y][x] === 0 || maze[y][x] === 1) {
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = 1;
-      ctx.strokeRect(x * boxSize, y * boxSize, boxSize, boxSize);
-    }
-    if (maze[y][x] === 1) {
-      ctx.drawImage(starimage, x * boxSize, y * boxSize, boxSize, boxSize);
-    } else if (maze[y][x] === 2) {
-      ctx.fillStyle = "#3374FF";
-      ctx.fillRect (x * boxSize, y * boxSize, boxSize, boxSize);
-    } else if (maze[y][x] === 3) {
-      ctx.drawImage(treeimage, x * boxSize, y * boxSize, boxSize, boxSize);
-    } else if (maze[y][x] === 4) {
-      ctx.drawImage(houseimage, x * boxSize, y * boxSize, boxSize, boxSize);
-    } else if (maze[y][x] === 5) {
-      ctx.drawImage(cityimage, x * boxSize, y * boxSize, boxSize, boxSize);
-    } else if (maze[y][x] === 6) {
-      ctx.drawImage(trophyimage, x * boxSize, y * boxSize, boxSize, boxSize);
-    } else if (maze[y][x] === 7) {
-      ctx.fillStyle = "#3374FF";
-      ctx.fillRect (x * boxSize, y * boxSize, boxSize, boxSize);
-      ctx.drawImage(bridgeimage, x * boxSize, y * boxSize, boxSize, boxSize);
+function drawMaze() {
+  for (var y = 0; y < maze.length; y++) {
+    for (var x = 0; x < maze[y].length; x++) {
+      if (maze[y][x] === 0 || maze[y][x] === 1) {
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 1;
+        ctx.strokeRect(x * boxSize, y * boxSize, boxSize, boxSize);
+      }
+      if (maze[y][x] === 1) {
+        ctx.drawImage(starimage, x * boxSize, y * boxSize, boxSize, boxSize);
+      } else if (maze[y][x] === 2) {
+        ctx.fillStyle = "#3374FF";
+        ctx.fillRect (x * boxSize, y * boxSize, boxSize, boxSize);
+      } else if (maze[y][x] === 3) {
+        ctx.drawImage(treeimage, x * boxSize, y * boxSize, boxSize, boxSize);
+      } else if (maze[y][x] === 4) {
+        ctx.drawImage(houseimage, x * boxSize, y * boxSize, boxSize, boxSize);
+      } else if (maze[y][x] === 5) {
+        ctx.drawImage(cityimage, x * boxSize, y * boxSize, boxSize, boxSize);
+      } else if (maze[y][x] === 6) {
+        ctx.drawImage(trophyimage, x * boxSize, y * boxSize, boxSize, boxSize);
+      }
     }
   }
 }
+
+function drawBridge(x, y) {
+  ctx.fillStyle = "#3374FF";
+  ctx.fillRect (x * boxSize, y * boxSize, boxSize, boxSize);
+  ctx.drawImage(bridgeimage, x * boxSize, y * boxSize, boxSize, boxSize);
+}
+
+drawMaze();
+
+// QUIZ
