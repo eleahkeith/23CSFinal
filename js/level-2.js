067074a571cf149e15299017179e50e66d1a2f39
fireonthesun <56504746+fireonthesun@users.noreply.github.com>
2020-06-27 10:43:27 +0100

    Updates to level 2

--- js/level-2.js
+++ js/level-2.js
@@ -6,16 +6,16 @@ var ctx = canvas.getContext("2d");
 const boxSize = canvas.width / 20;
 
 var state = {
-  question: 0,
-  character: {x: boxSize * 15, y: boxSize * 14},
-  question1XY: {x: boxSize * 17, y: boxSize * 10},
-  question2XY: {x: boxSize * 18, y: boxSize * 2},
-  question3XY: {x: boxSize * 15, y: boxSize * 5},
-  question4XY: {x: boxSize * 12, y: boxSize * 10},
-  question5XY: {x: boxSize * 2, y: boxSize * 13},
-  question6XY: {x: boxSize * 3, y: boxSize * 8},
-  question7XY: {x: boxSize * 4, y: boxSize * 3},
-  question8XY: {x: boxSize * 2, y: boxSize * 2},
+  question: 1,
+  character: { x: boxSize * 15, y: boxSize * 14 },
+  question1: { x: boxSize * 17, y: boxSize * 10, question: "Question 1", answers: ["1", "2", "3", "4"], correct: 2 },
+  question2: { x: boxSize * 18, y: boxSize * 2, question: "Question 2", answers: ["1", "2", "3", "4"], correct: 1 },
+  question3: { x: boxSize * 15, y: boxSize * 5, question: "Question 3", answers: ["1", "2", "3", "4"], correct: 4 },
+  question4: { x: boxSize * 12, y: boxSize * 10, question: "Question 4", answers: ["1", "2", "3", "4"], correct: 2 },
+  question5: { x: boxSize * 2, y: boxSize * 13, question: "Question 5", answers: ["1", "2", "3", "4"], correct: 3 },
+  question6: { x: boxSize * 3, y: boxSize * 8, question: "Question 6", answers: ["1", "2", "3", "4"], correct: 3 },
+  question7: { x: boxSize * 4, y: boxSize * 3, question: "Question 7", answers: ["1", "2", "3", "4"], correct: 2 },
+  question8: { x: boxSize * 2, y: boxSize * 2, question: "Question 8", answers: ["1", "2", "3", "4"], correct: 4 },
 }
 // Maze
 var maze = [
@@ -69,13 +69,15 @@ function drawMaze() {
   }
 }
 
-// Charactor movement
+// Charactor and movement
+var characterimage = document.querySelector("#characterimage");
+
 function drawCharacter() {
   ctx.drawImage(characterimage, state.character.x, state.character.y, boxSize, boxSize);
 }
 
 function moveUp(count) {
- state.character.y = state.character.y - boxSize * count;
+  state.character.y = state.character.y - boxSize * count;
 }
 
 function moveDown(count) {
@@ -89,22 +91,60 @@ function moveLeft(count) {
 function moveRight(count) {
   state.character.x = state.character.x + boxSize * count;
 }
-// function moveCharacter(question) {
-//   if (question === 1) {
-//    moveUp(3);
-//    moveRight(5);
-//  }
-// }
-//
-// // questions
-//
-//   let question = 1;
-//   moveCharacter(question);
+
+// Questions and anwser
+
+function setQuestionText(question) {
+  let questionText = document.getElementById("question");
+  let answer1 = document.getElementById("answer1");
+  let answer2 = document.getElementById("answer2");
+  let answer3 = document.getElementById("answer3");
+  let answer4 = document.getElementById("answer4");
+
+  questionText.innerText = question.question;
+  answer1.innerText = question.answers[0];
+  answer2.innerText = question.answers[1];
+  answer3.innerText = question.answers[2];
+  answer4.innerText = question.answers[3];
+}
+
+function getQuestion() {
+  switch (state.question) {
+    case 1:
+      setQuestionText(state.question1);
+      break;
+    case 2:
+      setQuestionText(state.question2);
+      break;
+    case 3:
+      setQuestionText(state.question3);
+      break;
+    case 4:
+      setQuestionText(state.question4);
+      break;
+    case 5:
+      setQuestionText(state.question5);
+      break;
+    case 6:
+      setQuestionText(state.question6);
+      break;
+    case 7:
+      setQuestionText(state.question7);
+      break;
+    case 8:
+      setQuestionText(state.question8);
+      break;
+    default:
+      setQuestionText(state.question1);
+
+  }
+}
 
 // Run game
 function runGame() {
   drawMaze();
   drawCharacter();
+  getQuestion();
 }
 
-setInterval(runGame, 50)
+setInterval(runGame, 50);
